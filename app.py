f5e0e6c0e14184cd4ac3c49ad20782ecdd1c3199
mamounjanfor <94909102+mamounjanfor@users.noreply.github.com>
2022-01-31 02:26:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return 'Hey, I am Mamoun Jnafor and This was my CI/CD project'
 
 
 if __name__=='__main__':
